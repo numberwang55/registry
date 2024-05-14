6ce76202138d404d1980da0f83d78eedf79b69e2
numberwang55 <cban0901@hotmail.com>
2024-05-14 14:20:49 +0100

    added navigation

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchInput() {
+  const [term, setTerm] = useState("");
+  const navigate = useNavigate()
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    navigate(`/search?term=${term}`)
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+      />
+    </form>
+  );
+}
