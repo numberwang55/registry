c6c0b0a40c06b8e99d3642c0dbce58400d3dbb82
numberwang55 <cban0901@hotmail.com>
2024-05-15 12:52:25 +0100

    created PackageListItem component, rendered in SearchPage & implemented styling

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,9 +1,18 @@
+import PackageListItem from "../../components/PackageListItem";
 import { SearchLoaderResult } from "./searchLoader";
 import { useLoaderData } from "react-router-dom";
 
 export default function SearchPage() {
   const { searchResults } = useLoaderData() as SearchLoaderResult;
-  console.log(searchResults);
 
-  return <div>Search Page</div>;
+  const renderedResults = searchResults.map((result) => {
+    return <PackageListItem _package={result} key={result.name} />;
+  });
+
+  return (
+    <div className="">
+      <h1 className="text-2xl font-bold my-6 ml-6">Search Results</h1>
+      <div className="space-y-4 mt-4">{renderedResults}</div>
+    </div>
+  );
 }
