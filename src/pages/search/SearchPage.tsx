254f4be2d871ec59e251a58470090c8d0f0686b0
numberwang55 <cban0901@hotmail.com>
2024-05-15 12:00:17 +0100

    implememnt SearchLoaderResult interface for searchLoader function and useLoaderData hook result in SearchPage

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,8 +1,9 @@
-import { useLoaderData } from "react-router-dom"
+import { SearchLoaderResult } from "./searchLoader";
+import { useLoaderData } from "react-router-dom";
 
 export default function SearchPage() {
-  const data = useLoaderData()
-  console.log(data);
-  
-  return <div>Search Page</div>
+  const { searchResults } = useLoaderData() as SearchLoaderResult;
+  console.log(searchResults);
+
+  return <div>Search Page</div>;
 }
