d250f5f2ea0d5490fe201f23ad4de7dc69276d09
numberwang55 <cban0901@hotmail.com>
2024-05-16 15:52:58 +0100

    detailsLoader

---
+++ src/pages/details/detailsLoader.ts
@@ -0,0 +1,17 @@
+import type { Params } from "react-router-dom";
+import { getPackage } from "../../api/queries/getPackage";
+
+export interface detailsLoaderResult {
+  
+}
+
+export async function detailsLoader({ params }: { params: Params }) {
+  const { name } = params;
+  if (!name) {
+    throw new Error("Name must be provided");
+  }
+  const details = await getPackage(name);
+  return {
+    details
+  };
+}
